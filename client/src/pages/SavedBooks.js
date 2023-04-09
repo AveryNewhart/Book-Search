9083a3f9ed1919631ab737b925674e7033a24495
AveryNewhart <newhartreeceavery@gmail.com>
2023-04-09 17:26:12 -0400

    was referencing the getme querie wrong

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,7 +7,7 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { GET_ME } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
