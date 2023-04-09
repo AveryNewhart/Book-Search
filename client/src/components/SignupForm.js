4970f00584e7b68e9726abb0cfcb64d620dadba3
AveryNewhart <newhartreeceavery@gmail.com>
2023-04-09 17:35:11 -0400

    reworked signupform to use the adduser mutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
  
 import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -14,7 +18,10 @@ const SignupForm = () => {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+
+    setUserFormData({ 
+      ...userFormData, 
+      [name]: value });
   };
 
   const handleFormSubmit = async (event) => {
@@ -28,15 +35,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const response = await addUser({
+        variables: { ...userFormData },
+      });
+      
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
